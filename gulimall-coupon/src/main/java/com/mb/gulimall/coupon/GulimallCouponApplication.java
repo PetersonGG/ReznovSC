fe44cc9ec30977922898514791838c97fd10569c
reznov <reznov198@163.com>
2022-05-07 22:06:35 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/main/java/com/mb/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.mb.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
